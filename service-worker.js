5bed6d12e8eaecc20f6d2ceae8f6013ed9638fc2
Iqbal333 <iqbalgooner33@gmail.com>
2020-11-12 22:01:05 +0700

    add node modules

--- service-worker.js
+++ service-worker.js
@@ -7,14 +7,33 @@ var urlsToCache = [
     "/pages/kuliner.html",
     "/pages/sejarah.html",
     "/pages/tentang.html",
-    "/css/materialize.min.css",
-    "/js/nav.js",
+    "/materialize/css/materialize.min.css",
+    "/materialize/js/nav.js",
 ];
 
 self.addEventListener("install", function(event) {
     event.waitUntil(
         caches.open(CACHE_NAME).then(function(cache) {
             return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener("fetch", function(event) {
+    event.respondWith(
+        caches
+        .match(event.request, { cacheName: CACHE_NAME })
+        .then(function(response) {
+            if(response) {
+                console.log("ServiceWorker: Gunakan aset dari cache: ", response.url);
+                return response;
+            }
+
+            console.log(
+                "ServiceWorker: Memuat aset dari server: ",
+                event.request.url
+            );
+            return fetch(event.request);
         })
     );
 });
